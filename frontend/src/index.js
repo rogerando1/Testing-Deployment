6dfb5881d66088dbeb820e97c1a2ba689a49c702
Roger R. Ando <139098175+rogerando1@users.noreply.github.com>
2023-11-23 11:55:46 +0800

    11

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,6 @@ import { StyledEngineProvider } from '@mui/material/styles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
   </React.StrictMode>
 );
 
